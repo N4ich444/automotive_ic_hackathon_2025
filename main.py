c241fd2a940e8e7bb048dac5e29cbfd7f34e6e46
Alan Kuang <akuang4@uwo.ca>
2025-03-09 11:01:57 -0400

    documentation

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import matplotlib.pyplot as plt
+# ALan Kuang 2025
+# Other Contributors put name here
+# Group 46 AIC 2025
+
 
 
 
